113a8be19304e63b3670e99b64aeb9bac1ea1deb
acamero <andrescamero@gmail.com>
2019-02-20 14:41:23 +0100

    IOHandler

--- examples/mnist/enqueue_task.js
+++ examples/mnist/enqueue_task.js
@@ -6,66 +6,44 @@
 *********************************************************************************************************************/
 
 var amqp = require('amqplib/callback_api');
+var tf = require('@tensorflow/tfjs');
+var ioh = require('./io_handler.js');
+// global.Blob = require('blob');
+// global.fetch = require('node-fetch');
+
 
 /*********************************************************************************************************************/
-/**
- * Creates a convolutional neural network (Convnet) for the MNIST data.
- *
- * @returns {tf.Model} An instance of tf.Model.
- */
-function createConvModel() {
-  // Create a sequential neural network model. tf.sequential provides an API
-  // for creating "stacked" models where the output from one layer is used as
-  // the input to the next layer.
-  const model = tf.sequential();
+const IMAGE_H = 28;
+const IMAGE_W = 28;
 
-  // The first layer of the convolutional neural network plays a dual role:
-  // it is both the input layer of the neural network and a layer that performs
-  // the first convolution operation on the input. It receives the 28x28 pixels
-  // black and white images. This input layer uses 16 filters with a kernel size
-  // of 5 pixels each. It uses a simple RELU activation function which pretty
-  // much just looks like this: __/
+function createConvModel() {
+  const model = tf.sequential();
   model.add(tf.layers.conv2d({
     inputShape: [IMAGE_H, IMAGE_W, 1],
     kernelSize: 3,
     filters: 16,
     activation: 'relu'
   }));
-
-  // After the first layer we include a MaxPooling layer. This acts as a sort of
-  // downsampling using max values in a region instead of averaging.
-  // https://www.quora.com/What-is-max-pooling-in-convolutional-neural-networks
   model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
-
-  // Our third layer is another convolution, this time with 32 filters.
   model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
-
-  // Max pooling again.
   model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
-
-  // Add another conv2d layer.
   model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
-
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
   model.add(tf.layers.flatten({}));
-
   model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-
-  // Our last layer is a dense layer which has 10 output units, one for each
-  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Here the classes actually
-  // represent numbers, but it's the same idea if you had classes that
-  // represented other entities like dogs and cats (two output classes: 0, 1).
-  // We use the softmax function as the activation for the output layer as it
-  // creates a probability distribution over our 10 classes so their output
-  // values sum to 1.
   model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
-
   return model;
 }
 
+async function setInitialModel(url) {
+  let model = createConvModel();
+  const saveResults = await model.save(ioh.webdisRequest(url)).catch(error => console.log(error));
+}
+
+async function loadModel(url) {
+  let model = await tf.loadModel(ioh.webdisRequest(url)).catch(error => console.log(error));
+}
 /*********************************************************************************************************************/
+
 
 // Parámetros de conexión
 let local = false;
@@ -77,7 +55,7 @@ if(local) {
   modelUrl = 'localhost:7379';
 } else {
   connStr = 'amqp://worker:mypassword@mallba3.lcc.uma.es';
-  modelUrl = 'mallba3.lcc.uma.es:7379';
+  modelUrl = 'http://mallba3.lcc.uma.es:7379';
 }
 console.log(connStr);
 
@@ -100,8 +78,10 @@ amqp.connect(connStr , function(err, conn) {
   conn.createChannel(function(err, ch) {    
     ch.assertQueue(destination, {durable: true});
     // Se encolan los mensajes en orden
-    // TODO: guardar versión inicial del modelo
     let modelId = 1;
+    // Guardamos una versión inicial del modelo
+    setInitialModel(modelUrl + "/SET/mnist_model_id_" + modelId);
+    // loadModel(modelUrl + "/GET/mnist_model_id_" + modelId);
     let awaitId = [];
     for(let i=1; i <= numSteps; i++) {
     // TODO: incluir tolerancia a fallos
@@ -114,7 +94,7 @@ amqp.connect(connStr , function(err, conn) {
       // contenido del mensaje.
       mapper.procId = "mapper_" + i;
       mapper.mapOrReduce = "map";
-      mapper.getModelUrl = modelUrl + "/model/id/" + modelId; 
+      mapper.getModelUrl = modelUrl + "/GET/mnist_model_id_" + modelId; 
       mapper.batchStep = i;
       console.log(mapper);
       ch.sendToQueue(destination, new Buffer(JSON.stringify(mapper)), {persistent: true});
@@ -125,14 +105,14 @@ amqp.connect(connStr , function(err, conn) {
         reducer.procId = "reducer_" + i;
         reducer.mapOrReduce = "reduce";
         reducer.awaitId = awaitId;
-        reducer.getModelUrl = modelUrl + "/model/id/" + modelId;
+        reducer.getModelUrl = modelUrl + "/GET/mnist_model_id_" + modelId;
         modelId++;
-        reducer.putModelUrl = modelUrl + "/model/id/" + modelId;
+        reducer.putModelUrl = modelUrl + "/SET/mnist_model_id_" + modelId;
         console.log(reducer);
         ch.sendToQueue(destination, new Buffer(JSON.stringify(reducer)), {persistent: true});
         awaitId.length = 0;
       }
     }
   });
-  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+  setTimeout(function() { conn.close(); process.exit(0); }, 500);
 });
