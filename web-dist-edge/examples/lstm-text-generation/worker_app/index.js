5de2aa09e2b14c10dd5bfda5e877c41ae009f206
Jose Ángel Morell <epimeteoypandora@gmail.com>
2019-03-06 21:23:35 +0100

    organizing library

---
+++ web-dist-edge/examples/lstm-text-generation/worker_app/index.js
@@ -0,0 +1,4 @@
+/*********************************************************************************************************************/
+/* TODO: Montar una página de bienvenida donde se muestre la URL para los workers, el estado actual del modelo
+/* y el progreso de las tareas. También se podría agregar la posibilidad de encolar más tareas.
+/*********************************************************************************************************************/
