f7a5c59728dfe3da9a304a6642d6224485d74790
jamorell <jamorell386@gmail.com>
2019-03-08 01:45:23 +0100

    loading data from url and getting batch using ID

--- web-dist-edge/examples/lstm-text-generation/tfjs_helper/data.js
+++ web-dist-edge/examples/lstm-text-generation/tfjs_helper/data.js
@@ -12,50 +12,80 @@ class TextDataset {
    *   sequence length expected by the LSTM model.
    * @param {number} sampleStep How many characters to skip when going from one
    *   example of the training data (in `textString`) to the next.
+   * @param {boolean} wordSet true = wordSet false = charSet.
    */
-  constructor(textString, sampleLen, sampleStep) {
+  constructor(textString, sampleLen, sampleStep, wordSet) {
     this.textString = textString;
     this.textLen = textString.length;
     this.sampleLen = sampleLen;
     this.sampleStep = sampleStep;
 
-    this.getCharSet();
-    this.convertAllTextToIndices();
-    this.generateExampleBeginIndices();
+    if (wordSet) {
+      this.getWordSet(textString);
+    } else {
+      this.getCharSet(textString);      
+    }
+    
+  }
+  //TODO -> Se pueden obtener los indices en un 2º metodo aunque para el ejemplo es
+  //suficiente. Igual con exampleBeginIndices.
+  getWordSet(textString) {
+    this.charSet = [];
+    this.indices = [];
+    this.words = textString.split(" ");
+    console.log("TOTAL WORDS "+this.words.length);
+    for (let i = 0; i < this.words.length; ++i) {
+      if (this.charSet.indexOf(this.words[i]) === -1) {
+        this.charSet.push(this.words[i]);
+      }
+      this.indices.push(this.charSet.indexOf(this.words[i]));
+    }
+    this.indices = new Uint16Array(this.indices);
+    console.log("TOTAL INDICES "+this.indices.length);   
+    
+    //    this.generateExampleBeginIndices();
+    // Prepare beginning indices of examples.
+    this.exampleBeginIndices = [];
+    for (let i = 0;
+        i < this.words.length - this.sampleLen - 1;
+        i += this.sampleStep) {
+      this.exampleBeginIndices.push(i);
+    }    
   }
 
-  getCharSet() {
+  //TODO -> Se pueden obtener los indices en un 2º metodo aunque para el ejemplo es
+  //suficiente. Igual con exampleBeginIndices.
+  getCharSet(textString) {
     this.charSet = [];
-    for (let i = 0; i < this.textLen; ++i) {
+    this.indices = [];
+    for (let i = 0; i < this.textString.length; ++i) {
       if (this.charSet.indexOf(this.textString[i]) === -1) {
         this.charSet.push(this.textString[i]);
       }
+      this.indices.push(this.charSet.indexOf(this.textString[i]));
     }
-    this.charSetSize = this.charSet.length;
+    this.indices = new Uint16Array(this.indices);  
+
+    //    this.generateExampleBeginIndices();
+    // Prepare beginning indices of examples.
+    this.exampleBeginIndices = [];
+    for (let i = 0;
+        i < this.textString.length - this.sampleLen - 1;
+        i += this.sampleStep) {
+      this.exampleBeginIndices.push(i);
+    }       
   }
 
-  convertAllTextToIndices() {
-    this.indices = new Uint16Array(this.textToIndices(this.textString));
-  }
 
-  textToIndices(text) {
-    const indices = [];
-    for (let i = 0; i < text.length; ++i) {
-      indices.push(this.charSet.indexOf(text[i]));
-    }
-    return indices;
-  }
-
-  nextDataBatch(batchSize) {
-    const xsBuffer = new tf.TensorBuffer([batchSize, this.sampleLen, this.charSetSize]);
-    const ysBuffer  = new tf.TensorBuffer([batchSize, this.charSetSize]);
+  getDataBatch(batchSize, batchIndex) {
+    const xsBuffer = new tf.TensorBuffer([batchSize, this.sampleLen, this.charSet.length]);
+    const ysBuffer  = new tf.TensorBuffer([batchSize, this.charSet.length]);
     for (let i = 0; i < batchSize; ++i) {
-      const beginIndex = this.exampleBeginIndices[this.examplePosition % this.exampleBeginIndices.length];
+      const beginIndex = this.exampleBeginIndices[batchIndex % this.exampleBeginIndices.length];
       for (let j = 0; j < this.sampleLen; ++j) {
         xsBuffer.set(1, i, j, this.indices[beginIndex + j]);
       }
       ysBuffer.set(1, i, this.indices[beginIndex + this.sampleLen]);
-      this.examplePosition++;
     }
     return [xsBuffer.toTensor(), ysBuffer.toTensor()];
   }
@@ -64,19 +94,11 @@ class TextDataset {
     return this.charSet[index];
   }
 
-  generateExampleBeginIndices() {
-    // Prepare beginning indices of examples.
-    this.exampleBeginIndices = [];
-    for (let i = 0;
-        i < this.textLen - this.sampleLen - 1;
-        i += this.sampleStep) {
-      this.exampleBeginIndices.push(i);
-    }
-
+  shuffleBeginIndices() {
     // Randomly shuffle the beginning indices.
     tf.util.shuffle(this.exampleBeginIndices);
-    this.examplePosition = 0;
   }
+  
 }
 
 module.exports.TextDataset = TextDataset;
