472948ffa93558c23a8a093c45bc99abcddbe132
acamero <andrescamero@gmail.com>
2019-04-02 13:27:01 +0200

    Eval TFJS model

--- jsdoop/examples/lstm-text-generation/tfjs-helper/tfjs_io_handler.js
+++ jsdoop/examples/lstm-text-generation/tfjs-helper/tfjs_io_handler.js
@@ -1,5 +1,5 @@
 const request = require('request');
-
+const fs = require('fs');
 const wdeUtils = require('jsd-utils');
 
 
@@ -52,3 +52,46 @@ function webdisRequest(path) {
 }
 
 module.exports.webdisRequest = webdisRequest;
+
+/**
+* JS Tensorflow IOHandler implementation to get a model from a webdis server
+*/
+class NodeFileRequest {   
+  constructor(path) {
+    this.path = path;
+  }
+
+  async save(modelArtifacts) {
+    let data = JSON.stringify([modelArtifacts.modelTopology, wdeUtils.ab2str(modelArtifacts.weightData), modelArtifacts.weightSpecs]);
+    fs.writeFile(this.path, data, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      console.log("The file was saved!");
+    }); 
+  }
+
+  async load() { 
+    let self = this; 
+    return new Promise(function(resolve, reject) {
+      fs.readFile(self.path, function(err, data) {
+        if (err) throw err;
+        let jsonBody = JSON.parse(data);                
+        let modelTopology = jsonBody[0];
+        let weightData = wdeUtils.str2ab(jsonBody[1]);
+        let weightSpecs = jsonBody[2];
+        resolve({modelTopology, weightSpecs, weightData});        
+      });	
+    });	
+  }
+}
+
+
+/**
+*
+*/
+function nodeFileRequest(path) {
+  return new NodeFileRequest(path);
+}
+
+module.exports.nodeFileRequest = nodeFileRequest;
